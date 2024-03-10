95f0cd68cbd8608807e6a656298a9d6665e2b7db
Corey <coreycr@hey.com>
2024-03-10 12:20:46 -0500

    Orders aisles alphabetically in Django admin

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class GroceryAisle(models.Model):
   name = models.CharField(max_length=100, blank=False, null=False, unique=True)
 
+  class Meta:
+    ordering = ['name']
+
   def __str__(self):
     return self.name
 
